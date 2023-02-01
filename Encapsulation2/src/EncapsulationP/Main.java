f184fd09a2e810987f0ed856c37e3e4c2ae567c1
Sayedkhater <khaterkhan@yahoo.com>
2023-02-01 18:46:07 -0500

    commit

---
+++ Encapsulation2/src/EncapsulationP/Main.java
@@ -0,0 +1,13 @@
+package EncapsulationP;
+
+public class Main {
+public static void main(String[] args) {
+	Students S= new Students();
+	S.setName("tommy");
+	S.setAge(24);
+	System.out.println(S.getName());
+	System.out.println(S.getAge());
+	
+	
+}
+}
